95975ec89efdd5965caf8bca58b20617753a7e12
rmccommon <mccommon@email.arizona.edu>
2019-04-04 12:08:11 -0700

    added game view thats not finished yet

--- src/game.rs
+++ src/game.rs
@@ -2,6 +2,11 @@ extern crate piston;
 extern crate glutin_window;
 extern crate graphics;
 extern crate opengl_graphics;
+use piston::window::WindowSettings;
+use piston::event_loop::{Events,EventLoop, EventSettings};
+use piston::input::RenderEvent;
+use glutin_window::GlutinWindow;
+use opengl_graphics::{OpenGL, GlGraphics};
 
 //! View for the game and its logic
 use crate::game_board;
@@ -9,5 +14,3 @@ use crate::game_board;
 pub stuct game_view{
     board: Board,
 }
-
-impl 
